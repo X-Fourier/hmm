78cfb473aa7625c6761fdbcc2641a42c973293ce
buildwu <lei_wu@fudan.edu.cn>
2015-04-17 16:46:08 +0800

    upload baum welch algorithm

--- src/hmm.cpp
+++ src/hmm.cpp
@@ -5,6 +5,10 @@
 #include <math.h>
 #include <algorithm>
 
+Hmm::Hmm() {
+
+}
+
 double Hmm::getTransProb(Transition *trans) {
     return _transition.get(trans->from()->state(), trans->to()->state());
 }
@@ -108,4 +112,155 @@ double Hmm::viterbi(std::vector<Transition*>& path) {
 
     std::reverse(path.begin(), path.end());
     return best->logAlpha();
+}
+
+// 将某个观察值加入到序列中
+void Hmm::addObs(unsigned long obs) {
+    std::vector<unsigned long> stateIds;
+    ULSet* cntx = _emission.getCntx(obs);
+
+    if (NULL == cntx) {
+        for (matrix2d::iterator it = _emission.begin(); it != _emission.end(); ++it) {
+            stateIds.push_back(it->first);
+        }
+    } else {
+        for (ULSet::iterator it = cntx->begin(); it != cntx->end(); ++it) {
+            stateIds.push_back(*it);
+        }
+    }
+
+    if (_timeSlots.empty()) {
+        TimeSlot* t0 = new TimeSlot;
+        t0->push_back(new ModelNode(0, _initState, this));
+        _timeSlots.push_back(t0);
+    }
+
+    TimeSlot* ts = new TimeSlot;
+    int time = _timeSlots.size();
+    for (uint32_t i = 0; i < stateIds.size(); i++) {
+        ModelNode* node = new ModelNode(time, stateIds[i], this);
+        ts->push_back(node);
+        TimeSlot* pre = _timeSlots[time-1];
+        for (TimeSlot::iterator it = pre->begin(); it != pre->end(); ++it) {
+            ULSet* possCntx = _transition.getCntx(node->state());
+            if (possCntx && possCntx->find((*it)->state()) != possCntx->end()) {
+                new Transition(*it, node, obs);
+            }
+        }
+    }
+    _timeSlots.push_back(ts);
+}
+
+// 计算观察序列的概率值
+double Hmm::obsProb() {
+    if (_timeSlots.size() < 1) {
+        return 1;
+    }
+    forward();
+    TimeSlot* last = _timeSlots[_timeSlots.size()];
+    std::vector<double> alphaT;
+    for (TimeSlot::iterator it = last->begin(); it != last->end(); ++it) {
+        alphaT.push_back((*it)->logAlpha());
+    }
+    return sumLogProb(alphaT);
+}
+
+// 根据观察序列对模型参数进行设置
+double Hmm::setModelParams(ModelParam& counts) {
+    double pOfObs = obsProb();
+    backward();
+
+    for (uint32_t t = 0; t < _timeSlots.size(); t++) {
+        TimeSlot* ts = _timeSlots[t];
+        TimeSlot::iterator it = ts->begin();
+        std::vector<double> logprobs;
+        for (; it != ts->end(); ++it) {
+            logprobs.push_back((*it)->logAlpha() + (*it)->logBeta());
+        }
+        double sum = sumLogProb(logprobs);
+
+        for (it = ts->begin(); it != ts->end(); ++it) {
+            ModelNode* node = *it;
+            double stateCount = node->logAlpha() + node->logBeta() - sum;
+            counts.stateCount().add(node->state(), stateCount);
+
+            std::vector<Transition*>& ins = node->ins();
+            uint32_t k;
+            for (k = 0; k < ins.size(); k++) {
+                Transition* trans = ins[k];
+                ModelNode* from = trans->from();
+                double transCount = from->logAlpha() + getTransProb(trans) + getEmitProb(trans) + node->logBeta() - pOfObs;
+                counts.emitCount().add(node->state(), trans->obs(), transCount);
+            }
+
+            std::vector<Transition*>& outs = node->outs();
+            for (k = 0; k < outs.size(); k++) {
+                Transition* trans = outs[k];
+                ModelNode* to = trans->to();
+                double transCount = node->logAlpha() + getTransProb(trans) + getEmitProb(trans) + to->logBeta() - pOfObs;
+                counts.transCount().add(node->state(), to->state(), transCount);
+            }
+        }
+    }
+
+    return pOfObs;
+}
+
+// 迭代过程中更新模型的参数
+void Hmm::updateParams(ModelParam& counts) {
+    _transition.clear();
+    _emission.clear();
+    
+    matrix2d::iterator it = counts.transCount().begin();
+    for (; it != counts.transCount().end(); ++it) {
+        unsigned long from = it->first;
+        double fromCount = counts.stateCount().get(from);
+        vector2d& cnts = *it->second;
+        for (vector2d::iterator itr = cnts.begin(); itr != cnts.end(); ++itr) {
+            _transition.add(from, itr->first, itr->second-fromCount);
+        }
+    }
+
+    for (it = counts.emitCount().begin(); it != counts.emitCount().end(); ++it) {
+        unsigned long state = it->first;
+        double stateCount = counts.stateCount().get(state);
+        vector2d& cnts = *it->second;
+        for (vector2d::iterator itr = cnts.begin(); itr != cnts.end(); ++itr) {
+            _emission.add(state, itr->first, itr->second-stateCount);
+        }
+    }
+}
+
+// 根据观察序列训练模型的EM算法
+void Hmm::baumWelch(const std::vector<std::vector<unsigned long>*>& seqs, int maxIters) {
+    double preTotalLogProb = 0;
+    for (int k = 0; k < maxIters; k++) {
+        ModelParam counts;
+        double totalLogProb = 0;
+        for (uint32_t i = 0; i < seqs.size(); i++) {
+            std::vector<unsigned long>& seq = *seqs[i];
+            for (uint32_t j = 0; j < seq.size(); j++) {
+                addObs(seq[i]);
+            }
+            totalLogProb += setModelParams(counts);
+            reset();
+        }
+        if (preTotalLogProb != 0 && (totalLogProb - preTotalLogProb < 1)) {
+            break;
+        } else {
+            preTotalLogProb = totalLogProb;
+        }
+        updateParams(counts);
+    }
+}
+
+void Hmm::reset() {
+    for (uint32_t t = 0; t < _timeSlots.size(); t++) {
+        delete _timeSlots[t];
+    }
+    _timeSlots.clear();
+}
+
+Hmm::~Hmm() {
+    reset();
 }
--- src/hmm.h
+++ src/hmm.h
@@ -8,6 +8,8 @@
 
 class Hmm {
 public:
+    Hmm();
+
     // 计算前向概率值
     void forward();
 
@@ -21,13 +23,24 @@ public:
     // 计算隐含状态序列的viterbi算法
     double viterbi(std::vector<Transition*>& path);
 
-    // 根据观察序列学习模型的EM算法
+    // 将某个观察值加入到序列中
+    void addObs(unsigned long obs);
+
+    // 根据观察序列训练模型的EM算法
     void baumWelch(const std::vector<std::vector<unsigned long>*>&, int);
 
-    // 更新模型参数
+    // 计算观察序列的概率值
+    double obsProb();
+
+    // 迭代过程中更新模型的参数
     void updateParams(ModelParam& counts);
 
-    double getModelParams(ModelParam& counts);
+    // 根据观察序列对模型参数进行设置
+    double setModelParams(ModelParam& counts);
+
+    void reset();
+    
+    ~Hmm();
 
 private:
     unsigned long _initState; // 初始状态值
@@ -37,8 +50,6 @@ private:
     matrix2d _emission; // 发射概率矩阵
 
     std::vector<TimeSlot*> _timeSlots; // 时间槽
-
-    double _minLogProb;
 };
 
 #endif
